9027d96622a16b71f57dd51053cba7eb1cb2aa44
Mészáros Bálint <minecraftgameplay6969@gmail.com>
2021-12-03 11:55:07 +0100

    Add files via upload

--- webanter/index.php
+++ webanter/index.php
@@ -4,35 +4,50 @@ include 'include/function/header.php';
 
 ?>
 <html>
-    <head>
-        <title>webanter</title>
-        
-        <link rel="stylesheet" type="text/css" href="css/index.css">
-        <script src="js/background.js" type="text/javascript"></script>
+<head>
+    <title>webanter</title>
 
-    </head>
-    <body onload="backgr()">
-        <header>
-            <div class="dropdown">
-                <button class="dropbtn">Language</button>
-                <div class="dropdown-content">
-                  <a href="?lang=hu"><img src="https://www.worldometers.info/img/flags/small/tn_hu-flag.gif" style="width: 65px; height: 40px;"></img></a>
-                  <a href="?lang=en"><img src="https://www.worldometers.info/img/flags/small/tn_uk-flag.gif" style="width: 65px; height: 40px;"></img></a>
-                </div>
-            </div>
-        </header>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="js/background.js" type="text/javascript"></script>
 
-        <div class="box">
-                    <h2><?= $lang['head'] ?></h2><br>
-                    <h2><?= $lang['text1'] ?></h2><br>
-                <div class="btn-group">
-                    <a href="pages/login.php"><button style="width:50%"><?= $lang['login'] ?></button></a>
-                    <a href="register.html"><button style="width:50%"><?= $lang['register'] ?></button></a>
-                </div>
+    <style type="text/css">
+        a:link {color: black;}      /* unvisited link */
+        a:visited {color: black;}   /* visited link */
+        a:hover {color: black;}     /* mouse over link */
+        a:active {color: black;}    /* selected link */
+    </style>
+
+</head>
+<body onload="backgr()">
+<header>
+    <div class="dropdown">
+        <button class="dropbtn">Language</button>
+        <div class="dropdown-content">
+            <a href="?lang=hu"><img src="https://www.worldometers.info/img/flags/small/tn_hu-flag.gif" alt="HU-flag" style="width: 65px; height: 40px;"></img></a>
+            <a href="?lang=en"><img src="https://www.worldometers.info/img/flags/small/tn_uk-flag.gif" alt="US-flag" style="width: 65px; height: 40px;"></img></a>
+        </div>
+    </div>
+</header>
+
+
+<div class="main">
+<div class="gradient-border" id="box">
+    <h2><?= $lang['head'] ?></h2><br>
+    <h2><?= $lang['text1'] ?></h2><br>
+
+        <div class="wrapper">
+            <a href="pages/login.php"><span><?= $lang['login'] ?></span></a>
         </div>
 
-        <footer class="container-fluid text-white" id="footer">
-                © 2021 MelodyZ
-        </footer>
-    </body>
+        <div class="wrapper">
+            <a href="pages/register.php"><span><?= $lang['register'] ?></span></a>
+        </div>
+
+</div>
+</div>
+
+<footer class="container-fluid text-white" id="footer">
+    © 2021 MelodyZ
+</footer>
+</body>
 </html>
